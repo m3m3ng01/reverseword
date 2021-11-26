8902a559d8d3f1679e1f573b2aa20ef3e520ba25
m3m3ng01 <antarsayuridn@gmail.com>
2021-11-26 19:25:01 +0700

    first commit

---
+++ ConsoleApp2/kebalikan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukkan kata yang akan dibalik: ");
+            string kata = Console.ReadLine();
+            string dibalik = string.Empty;
+
+             for (int i = kata.Length - 1;
+                i >= 0;
+                i--)
+            {
+                dibalik += kata[i];
+            }
+             Console.WriteLine($"kebalikan dari kata yang anda input adalah: " + dibalik);
+            Console.ReadKey();
+        }
+    }
+}
